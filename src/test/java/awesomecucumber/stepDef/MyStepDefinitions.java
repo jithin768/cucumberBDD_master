19a0fcef9f223ea25ee48d750aef3493102342b1
jithin768 <jithinmathew65@gmail.com>
2024-06-13 10:39:07 -0300

    adding Thread sleep

--- src/test/java/awesomecucumber/stepDef/MyStepDefinitions.java
+++ src/test/java/awesomecucumber/stepDef/MyStepDefinitions.java
@@ -11,11 +11,12 @@ import java.time.Duration;
 
 public class MyStepDefinitions {
     @Given("I'm on the store page1")
-    public void i_m_on_the_store_page1() {
+    public void i_m_on_the_store_page1() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://askomdch.com/store/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+        Thread.sleep(5000);
     }
     @When("I add a {string} to the cart1")
     public void i_add_a_to_the_cart1(String string) {
