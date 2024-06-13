376e387db1ff01b397ae16389312a0c6258117e4
Jithin Mathew <jithinmathew65@gmail.com>
2024-06-13 19:35:35 -0300

    Framework-initilization done via adding Driver factory and Hooks

--- src/test/java/awesomecucumber/stepDef/MyStepDefinitions.java
+++ src/test/java/awesomecucumber/stepDef/MyStepDefinitions.java
@@ -9,10 +9,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Sleeper;
 import org.testng.AssertJUnit;
 
 import java.time.Duration;
-
+import java.util.List;
+import java.util.Map;
 
 
 public class MyStepDefinitions {
@@ -35,9 +38,9 @@ public class MyStepDefinitions {
     }
     @Then("I should see {string} {string} in the cart1")
     public void i_should_see_in_the_cart1(String quantity, String productName) {
-        By productNameFld=By.cssSelector("");
+        By productNameFld=By.cssSelector("td[class='product-name'] a");
         String actualProductName=driver.findElement(productNameFld).getText();
-        By productQuantityFld=By.cssSelector("");
+        By productQuantityFld=By.cssSelector("input[type=\"number\"]");
         String actualQuantity=driver.findElement(productQuantityFld).getAttribute("value");
         Assert.assertEquals(productName,actualProductName);
         Assert.assertEquals(quantity,actualQuantity);
@@ -45,6 +48,7 @@ public class MyStepDefinitions {
 
     @Given("I'm a guest customer")
     public void i_m_a_guest_customer() {
+
         driver= DriverFactory.getDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
     }
@@ -62,22 +66,49 @@ public class MyStepDefinitions {
         driver.findElement(proceedToCheckoutBtn).click();
     }
     @When("I provide billing details")
-    public void i_provide_billing_details(io.cucumber.datatable.DataTable dataTable) {
+    public void i_provide_billing_details(List<Map<String, String>> billingDetails) {
 
-        By billingFirstnameFld=By.id()
+        By billingFirstnameFld=By.id("billing_first_name");
+        By billingLastNameFld=By.id("billing_last_name");
+        By billingAddressOneFld=By.id("billing_address_1");
+        By billingCityFld=By.id("billing_city");
+        By billingStateDropDown=By.id("billing_state");
+        By billingZipFld=By.id("billing_postcode");
+        By billingEmailFld=By.id("billing_email");
 
+        driver.findElement(billingFirstnameFld).clear();
+        driver.findElement(billingFirstnameFld).sendKeys(billingDetails.get(0).get("firstname"));
+        driver.findElement(billingLastNameFld).clear();
+        driver.findElement(billingLastNameFld).sendKeys(billingDetails.get(0).get("lastname"));
+        driver.findElement(billingAddressOneFld).clear();
+        driver.findElement(billingAddressOneFld).sendKeys(billingDetails.get(0).get("address_line1"));
+        driver.findElement(billingCityFld).clear();
+        driver.findElement(billingCityFld).sendKeys(billingDetails.get(0).get("city"));
+        //Select select=new Select(driver.findElement(billingStateDropDown));
+        //select.selectByVisibleText(billingDetails.get(0).get("state"));
+        driver.findElement(billingZipFld).clear();
+        driver.findElement(billingZipFld).sendKeys(billingDetails.get(0).get("zip"));
+        driver.findElement(billingEmailFld).clear();
+        driver.findElement(billingEmailFld).sendKeys(billingDetails.get(0).get("email"));
 
 
     }
     @When("I place an order")
-    public void i_place_an_order() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_place_an_order() throws InterruptedException {
+        System.out.println("Test1");
+        By placeOrderBtn=By.id("place_order");
+       System.out.println("Test1");
+        Thread.sleep(5000);
+       driver.findElement(placeOrderBtn).click();
+        Thread.sleep(5000);
+        driver.findElement(placeOrderBtn).click();
+
     }
     @Then("the order should be placed successfully")
     public void the_order_should_be_placed_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       By noticeTxt=By.cssSelector(".woocommerce-notice.woocommerce-notice--success.woocommerce-thankyou-order-received");
+       String actualNoticeMsg=driver.findElement(noticeTxt).getText();
+       Assert.assertEquals("Thank you. Your order has been received.", actualNoticeMsg);
     }
 
 }
